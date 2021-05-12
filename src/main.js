8257fa7b6b67c020fe86188b377d35bd0e9b76c8
Asmit Nepali <mail2asmitnepali@gmail.com>
2021-05-12 20:45:27 +0545

    Global Components.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseIcon from '@/components/BaseIcon.vue';
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App)
+app.component('BaseIcon',BaseIcon)
+app.use(store).use(router).mount("#app");
