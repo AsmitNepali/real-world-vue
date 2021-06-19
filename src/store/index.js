b32db7ed6476b8d9ecf72b4f6c61ec0213e71592
Asmit Nepali <mail2asmitnepali@gmail.com>
2021-06-19 12:38:18 +0545

    IMP: Vuex.

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    user:{'id':'abcd123', 'name':'Asmit Nepali'},
+    categories:[
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community',
+],
+  },
   mutations: {},
   actions: {},
   modules: {},
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="about">
-        <h1>Evet Create Page</h1>
+    <div>
+        <h1>Create Event, {{userName}}</h1>
+    <p>This is from {{ userName }}</p>
+    <ul>
+        <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
     </div>
 </template>
+
+<script>
+    import { mapState } from 'vuex'
+
+    export default {
+        computed:mapState({
+                userName: state => state.user.name,
+                userId: state => state.user.id,
+                categories: 'categories'
+        })
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
